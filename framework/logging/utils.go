178517727e8ccf009cfcad64999bede4597b5404
thomas-tacquet <thomas@tacquet.net>
2023-02-15 14:47:11 +0100

    clean server code unused

--- framework/logging/utils.go
+++ framework/logging/utils.go
@@ -1,18 +1,1 @@
 package logging
-
-import (
-	"fmt"
-	"strings"
-)
-
-func FormatLevel(i interface{}) string {
-	if i == nil {
-		i = "DEBUG"
-	}
-	return strings.ToUpper(fmt.Sprintf("%-5s -", i))
-}
-
-func FormatTimestamp(i interface{}) string {
-	// we don't want to display timestamp in the log entry
-	return ""
-}
