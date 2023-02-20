e1d7094b593bf880d5c66a17f4dbbac17524081f
thomas-tacquet <thomas@tacquet.net>
2023-02-20 16:43:08 +0100

    review

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/scaleway/serverless-functions-go
 
-go 1.17
+go 1.18
 
 require (
 	github.com/google/uuid v1.3.0
--- testing/infra.go
+++ testing/infra.go
@@ -1,11 +1,25 @@
 package testing
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 // InfraProcessing simulates the infrastructure layer where your FaaS will be deployed.
-func InfraProcessing(httpResp http.ResponseWriter, httpReq *http.Request) {
-	httpResp.Header().Add("X-Forwarded-For", httpReq.RemoteAddr)
-	httpResp.Header().Add("X-Forwarded-Proto", "https")
-	httpResp.Header().Add("X-Envoy-External-Address", httpReq.RemoteAddr)
-	httpResp.Header().Add("x-envoy-upstream-service-time", "0")
+func InfraProcessing(httpReq *http.Request) {
+	reqId, err := uuid.NewUUID()
+	if err != nil {
+		panic(err)
+	}
+
+	// headers for function
+	httpReq.Header.Add("Forwarded", "for="+httpReq.RemoteAddr+";proto=http")
+	httpReq.Header.Add("K-Proxy-Request", "activator")
+	httpReq.Header.Add("X-Forwarded-For", httpReq.RemoteAddr)
+	httpReq.Header.Add("X-Forwarded-For", "127.0.0.1")
+	httpReq.Header.Add("X-Forwarded-For", "127.0.0.2")
+	httpReq.Header.Add("X-Forwarded-Proto", "http")
+	httpReq.Header.Add("X-Request-Id", reqId.String())
+	httpReq.Header.Add("X-Envoy-External-Address", httpReq.RemoteAddr)
 }
