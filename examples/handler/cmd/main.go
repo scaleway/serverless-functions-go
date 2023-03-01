cf9bc34c199daba650caf961a7e7cc03bf276838
thomas-tacquet <thomas@tacquet.net>
2023-03-01 14:59:45 +0100

    update example deps

--- examples/handler/cmd/main.go
+++ examples/handler/cmd/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	scw "github.com/scaleway/serverless-functions-go/examples/handler"
-	server "github.com/scaleway/serverless-functions-go/testing"
+	"github.com/scaleway/serverless-functions-go/functest"
 )
 
 func main() {
 	// Replace "Handle" with your function handler name if necessary
-	server.ServeHandlerLocally(scw.Handle, server.WithPort(8080))
+	functest.ServeHandlerLocally(scw.Handle, functest.WithPort(8080))
 }
