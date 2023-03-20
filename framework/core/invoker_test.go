694bed857bbc5b835f114dda767f7f98a1efa7ee
Thomas TACQUET <thomas@tacquet.net>
2023-03-20 19:53:22 +0100

    Update framework/core/invoker_test.go

--- framework/core/invoker_test.go
+++ framework/core/invoker_test.go
@@ -58,7 +58,7 @@ func TestExecutePath(t *testing.T) {
 	t.Parallel()
 
 	const path = "/test/path"
-	original, err := http.NewRequest(http.MethodGet, path, http.NoBody)
+	original, err := http.NewRequestWithContext(context.Background(), http.MethodGet, path, http.NoBody)
 	require.NoError(t, err)
 	assert.Equal(t, path, original.URL.Path)
 
